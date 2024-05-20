cc13d742e703300fd36a9717584ba54887233e41
Michael Bestas <mkbestas@gmail.com>
2024-05-21 02:29:47 +0300

    Extract additional files to factory directory
    Change-Id: Ie6b78a12a3a350ffb7667f1fb85675a6594a5c8a

--- extract-files.py
+++ extract-files.py
@@ -45,6 +45,7 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     extract_fns=extract_fns,
     add_firmware_proprietary_file=True,
+    add_factory_proprietary_file=True,
     add_generated_carriersettings=True,
 )
 
