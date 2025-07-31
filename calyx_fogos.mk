fb8bfe4266718e4d1615e24a7a971e5fef8c85ea
Michael Bestas <mkbestas@gmail.com>
2025-07-31 18:10:39 +0300

    calyx: Update to V1UGS35H.75-14-3-3
    Change-Id: I01ce76058199ce4f3c2a26a6de9863d605bbdc73

--- calyx_fogos.mk
+++ calyx_fogos.mk
@@ -21,6 +21,6 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g34 5G
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="fogos_g-user 15 V1UG35H.75-14-3 6c77f4-ac72a9 release-keys MV-186" \
-    BuildFingerprint=motorola/fogos_g/fogos:15/V1UG35H.75-14-3/6c77f4-ac72a9:user/release-keys \
+    BuildDesc="fogos_g-user 15 V1UGS35H.75-14-3-3 42ab99-25f36e release-keys MV-186" \
+    BuildFingerprint=motorola/fogos_g/fogos:15/V1UGS35H.75-14-3-3/42ab99-25f36e:user/release-keys \
     DeviceProduct=fogos_g
